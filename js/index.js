35b5f9e35dbb9450a1fe0e205d3f5b9e0b2726b0
hello_my <hello_my@126.com>
2022-01-03 21:29:42 +0800

    我的作品显示有没有发布

--- js/index.js
+++ js/index.js
@@ -69,29 +69,41 @@ Vue.component('s-c2', {
 `
 })
 Vue.component('s-workcard',{
-    props:['work','user','host'],
+    props:['work','user','host','my'],
     template:`<v-card :href="'#page=work&id='+work.id">
-    <v-img :src="host.data+'/static/internalapi/asset/'+work.image" :aspect-ratio="4/3" class="white--text align-end"
-        gradient="to bottom, rgba(0,0,0,0) 80%, rgba(0,0,0,.6)">
-        <span class="ma-2">
-            <span>
-                <v-icon color="white" size="16">mdi-eye</v-icon> {{ work.look }}
+    <span>
+        <v-img :src="host.data+'/static/internalapi/asset/'+work.image" :aspect-ratio="4/3"
+            class="white--text align-end" gradient="to bottom, rgba(0,0,0,0) 80%, rgba(0,0,0,.6)">
+            <span class="ma-2">
+                <span>
+                    <v-icon color="white" size="16">mdi-eye</v-icon> {{ work.look }}
+                </span>
+                <span class="ml-1">
+                    <v-icon color="white" size="16">mdi-heart</v-icon> {{ work.like }}
+                </span>
             </span>
-            <span class="ml-1">
-                <v-icon color="white" size="16">mdi-heart</v-icon> {{ work.like }}
-            </span>
+        </v-img>
+        <div class="text-truncate px-5 mt-3 mb-1 text-heading-6">{{ work.name }}</div>
+        <span class="px-5" v-if="user">
+            <v-avatar size="25">
+                <img
+                    :src="host.data+'/static/internalapi/asset/'+(user.head || '6e2b0b1056aaa08419fb69a3d7aa5727.png')">
+            </v-avatar>
+            <span color="accent" class="text-subtitle-1 text--secondary text-truncate text-caption"
+                :href="'#page=user&id='+work.author">{{
+                user.nickname }}</span><br>
         </span>
-    </v-img>
-    <div class="text-truncate px-5 mt-3 mb-1 text-heading-6">{{ work.name }}</div>
-    <span class="px-5" v-if="user">
-        <v-avatar size="25">
-            <img :src="host.data+'/static/internalapi/asset/'+(user.head || '6e2b0b1056aaa08419fb69a3d7aa5727.png')">
-        </v-avatar>
-        <span color="accent" class="text-subtitle-1 text--secondary text-truncate text-caption"
-            :href="'#page=user&id='+work.author">{{
-            user.nickname }}</span><br>
+        <span else><br></span>
     </span>
-    <span else><br></span>
+    <span>
+        <v-btn color="green" text v-if="work.publish" depressed block text tile>已发布
+        </v-btn>
+        <v-btn color="red" text v-else depressed block text tile>未发布
+        </v-btn>
+    </span>
+    <v-btn color="accent" class="" v-if="my" :href="'/other/scratch.html#id='+work.id" target="_blank" depressed block
+        tile>继续创作
+    </v-btn>
 </v-card>`
 })
 window.alert = (text, timeout) => {
